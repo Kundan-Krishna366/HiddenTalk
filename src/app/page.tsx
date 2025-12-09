c13c6cc8579c1dee5f1870f2a07679c38c0ef538
kundan-krishna366 <kundan.krishna366@gmail.com>
2025-12-09 22:20:51 +0530

    Remove default README content and implement basic layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className=""></div>
+    </main>
   );
 }
